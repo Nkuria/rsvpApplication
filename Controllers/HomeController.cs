f682d9e706e9b5eef10f0b897330aa83dd88efab
Nick Kuria <nkuria01@gmail.com>
2022-06-02 08:40:09 +0300

    Added the Repository class to hold temporary values

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,15 +12,23 @@ namespace rsvpApplication.Controllers
     public class HomeController : Controller
     {
      
-
+        
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+
         }
 
       
---
+++ Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+namespace rsvpApplication.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+
+    }
+}
